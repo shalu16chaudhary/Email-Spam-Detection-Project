388cac9f5a651e721a5d7a55079f91ca546bc2f6
shalu16chaudhary <shaluchaudhary1629@gmail.com>
2025-07-28 12:04:45 -0700

    Final deploy fixes

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def predict():
     return render_template('index.html', prediction=result, message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
